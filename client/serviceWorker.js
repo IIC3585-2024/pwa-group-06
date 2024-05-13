927d004809acd1cc6419eb3d090f781680b07601
Carlos Paredes <carlos@paredesrobles.com>
2024-05-13 04:56:13 -0400

    chore(): remove unused code

--- client/serviceWorker.js
+++ client/serviceWorker.js
@@ -1,27 +1,35 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
-
   const CORE_STATIC_FILES = [
-    '/',
-    '/index.html'
+    '/index.html',
+    '/notebook.html'
   ];
 
   workbox.precaching.precacheAndRoute(CORE_STATIC_FILES);
 
-  workbox.routing.registerNavigationRoute(
-    workbox.precaching.getCacheKeyForURL('/index.html'), {
-      whitelist: ['^/notebooks', '^/notes'],
-    }
+  workbox.routing.registerRoute(
+    new RegExp('/index.html'),
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'html-cache'
+    })
   );
 
   workbox.routing.registerRoute(
-		new RegExp('.*\.js'),
-		new workbox.strategies.StaleWhileRevalidate({
-			cacheName: 'javascript',
-		})
-	);
+    new RegExp('/notebook.html'),
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'html-cache'
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'javascript',
+    })
+  );
 
   workbox.routing.registerRoute(
 		new RegExp('.*\.css'),
@@ -29,6 +37,13 @@ if (workbox) {
 			cacheName: 'css',
 		})
 	);
+
+  workbox.routing.registerRoute(
+    new RegExp('(.*)/notepads'),
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'notepads',
+    })
+  );
 
   workbox.routing.registerRoute(
 		/\.(?:png|gif|jpg|jpeg|svg)$/,
